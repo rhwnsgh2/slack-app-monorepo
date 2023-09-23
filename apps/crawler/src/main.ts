41e03a1cd4bd6240cd92b49c5cabd573a7516cf1
junho <junho.ko@business-canvas.com>
2023-09-23 12:01:04 +0900

    feat: add crawler test

--- apps/crawler/src/main.ts
+++ apps/crawler/src/main.ts
@@ -1,1 +1,34 @@
-console.log('Hello World');
+import axios from 'axios';
+
+const getChannelList = async () => {
+  const res = await axios.get('https://slack.com/api/conversations.list', {
+    headers: {
+      Authorization: `Bearer ${process.env.SLACK_USER_TOKEN}`,
+    },
+  });
+
+  return res.data.channels;
+};
+
+const getChannelHistory = async (channelId: string) => {
+  const res = await axios.get('https://slack.com/api/conversations.history', {
+    headers: {
+      Authorization: `Bearer ${process.env.SLACK_USER_TOKEN}`,
+    },
+    params: {
+      channel: channelId,
+    },
+  });
+
+  return res.data.messages;
+};
+
+const main = async () => {
+  const channels = await getChannelList();
+
+  const channelHistory = await getChannelHistory(channels[0].id);
+
+  console.log(channelHistory[0]);
+};
+
+main();
