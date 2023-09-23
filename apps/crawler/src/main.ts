37198c880d39e4cdccdf6507a63dbae9b42bc5bf
junho <junho.ko@business-canvas.com>
2023-09-23 16:28:35 +0900

    feat: add thread pagination

--- apps/crawler/src/main.ts
+++ apps/crawler/src/main.ts
@@ -1,25 +1,50 @@
 import axios from 'axios';
 import getChannelList from './api/getChannelList';
+import { Thread } from './types/thread';
 
-const getChannelHistory = async (channelId: string) => {
+const getChannelHistory = async (
+  channelId: string,
+  threads: Thread[] = [],
+  cursor?: string,
+) => {
   const res = await axios.get('https://slack.com/api/conversations.history', {
     headers: {
       Authorization: `Bearer ${process.env.SLACK_USER_TOKEN}`,
     },
     params: {
       channel: channelId,
+      cursor,
     },
   });
 
-  return res.data.messages;
+  const responseThreads = res.data.messages.map((message) => {
+    return {
+      id: message.ts,
+      channel: channelId,
+      text: message.text,
+      user: message.user,
+    };
+  });
+
+  if (res.data.has_more) {
+    const nextMessages = await getChannelHistory(
+      channelId,
+      [...threads, ...res.data.messages],
+      res.data.response_metadata.next_cursor,
+    );
+
+    return [...responseThreads, ...nextMessages];
+  }
+
+  return [...threads, ...responseThreads];
 };
 
 const main = async () => {
   const channels = await getChannelList();
 
-  const channelHistory = await getChannelHistory(channels[0].id);
+  const channelHistory = await getChannelHistory(channels[3].id);
 
-  console.log(channelHistory);
+  console.log(channelHistory[0]);
 };
 
 main();
---
+++ apps/crawler/src/types/thread.ts
@@ -0,0 +1,6 @@
+export interface Thread {
+  id: string;
+  channel: string;
+  text: string;
+  user: string;
+}
